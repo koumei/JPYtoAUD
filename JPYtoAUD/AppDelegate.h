552b2183884300a842e3bea60efbf3c2276af392
koumei <koumeibb@gmail.com>
2014-03-10 22:36:17 +1100

    Initial Commit

---
+++ JPYtoAUD/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  JPYtoAUD
+//
+//  Created by K on 10/03/14.
+//  Copyright (c) 2014 koumei.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ JPYtoAUDTests/JPYtoAUDTests.m
@@ -0,0 +1,34 @@
+//
+//  JPYtoAUDTests.m
+//  JPYtoAUDTests
+//
+//  Created by K on 10/03/14.
+//  Copyright (c) 2014 koumei.net. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JPYtoAUDTests : XCTestCase
+
+@end
+
+@implementation JPYtoAUDTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
